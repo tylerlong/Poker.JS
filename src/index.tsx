e41732f53fa1397bf7b65927b95b82f4d698ddb9
Tyler Liu <tyler4long@gmail.com>
2024-05-03 21:45:32 -0700

    Update demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import Poker from './poker';
 
+// regular
 document.body.appendChild(Poker.getCardImage(256, 'hearts', 'Q'));
 document.body.appendChild(Poker.getCardImage(256, 'clubs', 'A'));
+document.body.appendChild(Poker.getCardImage(256, 'diamonds', '6'));
+document.body.appendChild(Poker.getCardImage(256, 'hearts', '8'));
+
+// joker
+document.body.appendChild(Poker.getCardImage(256, 'spades', 'O'));
+document.body.appendChild(Poker.getCardImage(256, 'hearts', 'O'));
+
+// back
 document.body.appendChild(Poker.getBackImage(256, '#BB5555', '#AA2222'));
+document.body.appendChild(Poker.getBackImage(256, '#F9CD9C', '#EB870D'));
+document.body.appendChild(Poker.getBackImage(256, '#A464B8', '#5E0C78'));
 document.body.appendChild(Poker.getBackImage(256, '#7A7BB8', '#2E319C'));
+document.body.appendChild(Poker.getBackImage(256, '#58AAAF', '#1F7A80'));
+document.body.appendChild(Poker.getBackImage(256, '#78A867', '#437034'));
+document.body.appendChild(Poker.getBackImage(256, '#B7B860', '#7E7F2A'));
+document.body.appendChild(Poker.getBackImage(256, '#A9566C', '#71273A'));
+document.body.appendChild(Poker.getBackImage(256, '#8BA23C', '#3C451B'));
+document.body.appendChild(Poker.getBackImage(256, '#5C72C2', '#2B4299'));
+document.body.appendChild(Poker.getBackImage(256, '#90928C', '#535550'));
